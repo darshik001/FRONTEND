ed6baaa00332f20ac2a03d8bfa037a5bb425934f
darshik007 <darshikshekhada07@gmail.com>
2025-08-12 14:27:11 +0530

    12/08/25

--- PROJECT/DSA-PROJECT/project-2/script.js
+++ PROJECT/DSA-PROJECT/project-2/script.js
@@ -1,87 +1,78 @@
-
-
 // 1.WAP for Circular Linklist Operation (Insert, delete, display)
-     class cnode{
-      constructor(data){
-        this.data = data;
-        this.next = null
-      }
-     }
-
-     class circularList {
-constructor(){
-  this.head = null
-  this.last = null
+class cnode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
-  apddend(val){
-    let temp = new cnode(val)
-      if(this.head == null){
-        this.head = temp
-        this.last = temp
-        this.last.next = this.head
-      } else{
-        this.last.next= temp
-        temp.next = this.head
-        this.last = temp
+class circularList {
+  constructor() {
+    this.head = null;
+    this.last = null;
+  }
+
+  apddend(val) {
+    let temp = new cnode(val);
+    if (this.head == null) {
+      this.head = temp;
+      this.last = temp;
+      this.last.next = this.head;
+    } else {
+      this.last.next = temp;
+      temp.next = this.head;
+      this.last = temp;
+    }
+  }
+
+  deleteData() {
+    let currnt = this.head;
+    let previus = null;
+
+    if (this.head == this.last) {
+      this.head = null;
+      this.last = null;
+    } else {
+      while (currnt !== this.last) {
+        previus = currnt;
+        currnt = currnt.next;
       }
+
+      previus.next = this.head;
+      this.last = previus;
+    }
   }
 
-  deleteData(){
-    let currnt = this.head
-    let previus = null
-     
-        if(this.head == this.last){
-          this.head = null
-          this.last = null
-        }else{
-
-          
-          while(currnt !== this.last){
-            previus = currnt
-            currnt = currnt.next
-          }
-          
-          previus.next = this.head
-          this.last = previus
-        }
-   
-  }
-
-  displayData(){
-    let currnt = this.head
-    if(this.head == null){
-      console.log("List Empty")
-      return
+  displayData() {
+    let currnt = this.head;
+    if (this.head == null) {
+      console.log("List Empty");
+      return;
     }
 
-    do{
-      console.log(currnt.data)
-      currnt = currnt.next
-    } while(currnt !== this.head)
+    do {
+      console.log(currnt.data);
+      currnt = currnt.next;
+    } while (currnt !== this.head);
   }
-     }
+}
 
+let circular = new circularList();
+circular.apddend(10);
+circular.apddend(20);
+circular.apddend(30);
+circular.apddend(40);
+circular.apddend(50);
+circular.deleteData();
+circular.deleteData();
 
-let circular = new circularList()
-circular.apddend(10)
-circular.apddend(20)
-circular.apddend(30)
-circular.apddend(40)
-circular.apddend(50)
-circular.deleteData()
-circular.deleteData()
+circular.displayData();
 
-circular.displayData()
-
-// OUTPUT 
+// OUTPUT
 
 // 30
 // 10
 // 20
-
-
-
 
 
 
@@ -100,7 +91,6 @@ class siglyLink {
     this.head = null;
   }
 
-  
   addataend(val) {
     let temp = new node(val);
 
@@ -115,8 +105,6 @@ class siglyLink {
       current.next = temp;
     }
   }
-
- 
 
   // PALINDROM  LIST OR NOT
 
@@ -145,34 +133,31 @@ class siglyLink {
   //  REVERS LIST
 
   reverseList() {
+    let end = this.head;
+    while (end.next !== null) {
+      end = end.next;
+    }
 
-      let end = this.head;
-      while (end.next !== null) {
-          end = end.next;
-      }
-
-      while (this.head !== end) {
-          let current = this.head;
-          this.head = this.head.next;
-          current.next = end.next;
-          end.next = current;
-      }
+    while (this.head !== end) {
+      let current = this.head;
+      this.head = this.head.next;
+      current.next = end.next;
+      end.next = current;
+    }
   }
-
-  
 
   // DISPLAY DATA
   displayData() {
     if (this.head == null) {
       console.log("List Is Empty");
     } else {
-      let listdata  = "";
+      let listdata = "";
       let current = this.head;
       while (current) {
-        listdata += current.data + "->"
+        listdata += current.data + "->";
         current = current.next;
       }
-      console.log(listdata)
+      console.log(listdata);
     }
   }
 }
@@ -188,11 +173,9 @@ Singly.addataend(50);
 Singly.reverseList();
 Singly.displayData();
 
-// OUTPUT 
+// OUTPUT
 
 // 50->40->30->20->10->
-
-
 
 
 
@@ -211,7 +194,6 @@ class palindrom {
     this.head = null;
   }
 
-  
   addataend(val) {
     let temp = new pnode(val);
 
@@ -226,8 +208,6 @@ class palindrom {
       current.next = temp;
     }
   }
-
- 
 
   // PALINDROM  LIST OR NOT
 
@@ -253,11 +233,6 @@ class palindrom {
     return true;
   }
 
-
-
-
-  
-
   // DISPLAY DATA
   displayData() {
     if (this.head == null) {
@@ -280,17 +255,15 @@ palindromcheck.addataend(30);
 palindromcheck.addataend(20);
 palindromcheck.addataend(10);
 
-
-if(palindromcheck.ispalindrom() === true){
-  console.log("This List Is Palindrom")
-}else{
-  console.log("This List Is Not Palindrom")
+if (palindromcheck.ispalindrom() === true) {
+  console.log("This List Is Palindrom");
+} else {
+  console.log("This List Is Not Palindrom");
 }
-
 
 palindromcheck.displayData();
 
-// OUTPUT 
+// OUTPUT
 
 // This List Is Palindrom
 // 10
