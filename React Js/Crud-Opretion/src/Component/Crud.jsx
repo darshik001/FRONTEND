341c5699d6ec6eafb924f8bb8eb25bad8f6914db
darshik007 <darshikshekhada07@gmail.com>
2025-10-08 17:31:53 +0530

    08/10/25

---
+++ React Js/Crud-Opretion/src/Component/Crud.jsx
@@ -0,0 +1,164 @@
+import { Col, Container, Form, Row, Button, InputGroup } from "react-bootstrap";
+import { FaTags, FaTag, FaDollarSign, FaBoxes, FaImage } from "react-icons/fa";
+import "./Crud.css";
+import uniqueId from 'generate-unique-id'
+import { useState } from "react";
+
+const Crud = () => {
+  const initialvalue ={
+    id:"",
+    category:"",
+    name:"",
+    price:"",
+    stock:"",
+    image:""
+  }
+  const [inputForm,setinputForm] = useState(initialvalue)
+  const [inputErr,setinputErr] = useState({})
+
+  const handalchang = (e)=>{
+    const {name,value} = e.target
+
+    setinputForm({
+      ...inputForm,
+      [name] :value
+    })
+  }
+
+
+  const handalsubmit =(e) =>{
+    e.preventDefault()
+
+    if(handalErros()){
+     inputForm.id  ="DL"+ uniqueId({
+      length:10,
+       useLetters:false,
+       includeSymbols:['@','#','$']
+     }) 
+      console.log(inputForm)
+      setinputForm(initialvalue) 
+    }
+  }
+
+  const handalErros = ()=>{
+    let errors = {}
+
+    if(inputForm.category === ""){
+      errors.categoryErr = "Plase select category"
+    }
+
+    if(inputForm.name == ""){
+      errors.nameErr = "Enter Product Name"
+    }
+
+    if(inputForm.price == 0){
+      errors.priceErr = "Enter Price"
+    }
+
+    if(inputForm.stock==0){
+      errors.stockErr = "Enter stock"
+    }
+
+    if(inputForm.image ==""){
+      errors.imageErr = "Provied Image Url"
+    }
+
+    setinputErr(errors)
+    return Object.keys(errors).length === 0
+  }
+  return (
+    <Container fluid className="crud-container d-flex justify-content-center align-items-center">
+      <Row className="justify-content-center">
+        <Col >
+          <div className="form-card rounded-5 w-100 p-5 bg-white">
+            <div className="form-header text-center mb-5">
+              <h2 className="fw-bold h2">Add New Product</h2>
+              <hr />
+            </div>
+
+            <Form onSubmit={handalsubmit}>
+              <Row>
+                {/* Category */}
+                <Col md={6} className="mb-3">
+                  <Form.Label>
+                    Category <span className="text-danger">*</span>
+                  </Form.Label>
+                  <InputGroup>
+                    <InputGroup.Text><FaTags /></InputGroup.Text>
+                    <Form.Select name="category" value={inputForm.category} onChange={handalchang}>
+                      <option value="">Select Category</option>
+                      <option value="Clothing">Clothing</option>
+                      <option value="Electronic">Electronic</option>
+                      <option value="Grosery">Grosery</option>
+                      <option value="Jewellery">Jewellery</option>
+                    </Form.Select>
+                  </InputGroup>
+                   {inputErr.categoryErr? <Form.Text className="text-danger">{inputErr.categoryErr}</Form.Text>:""}
+                </Col>
+
+                {/* Name */}
+                <Col md={6} className="mb-3">
+                  <Form.Label>
+                    Product Name <span className="text-danger">*</span>
+                  </Form.Label>
+                  <InputGroup>
+                    <InputGroup.Text><FaTag /></InputGroup.Text>
+                    <Form.Control type="text" name="name" value={inputForm.name} onChange={handalchang} />
+                  </InputGroup>
+                    {inputErr.nameErr? <Form.Text className="text-danger">{inputErr.nameErr}</Form.Text>:""}
+                </Col>
+              </Row>
+
+              <Row>
+                {/* Price */}
+                <Col md={6} className="mb-3">
+                  <Form.Label>
+                    Price <span className="text-danger">*</span>
+                  </Form.Label>
+                  <InputGroup>
+                    <InputGroup.Text><FaDollarSign /></InputGroup.Text>
+                    <Form.Control type="number" name="price" value={inputForm.price} onChange={handalchang} />
+                  </InputGroup>
+                  {inputErr.priceErr? <Form.Text className="text-danger">{inputErr.priceErr}</Form.Text>:""}
+                </Col>
+
+                {/* Stock */}
+                <Col md={6} className="mb-3">
+                  <Form.Label>
+                    Stock <span className="text-danger">*</span>
+                  </Form.Label>
+                  <InputGroup>
+                    <InputGroup.Text><FaBoxes /></InputGroup.Text>
+                    <Form.Control type="number" name="stock" value={inputForm.stock} onChange={handalchang} />
+                  </InputGroup>
+                  {inputErr.stockErr? <Form.Text className="text-danger">{inputErr.stockErr}</Form.Text>:""}
+                </Col>
+              </Row>
+
+              {/* Image */}
+              <Form.Group controlId="image" className="mb-4">
+                <Form.Label>
+                  Image URL <span className="text-danger">*</span>
+                </Form.Label>
+                <InputGroup>
+                  <InputGroup.Text><FaImage /></InputGroup.Text>
+                  <Form.Control type="text" name="image" value={inputForm.image} onChange={handalchang} />
+                </InputGroup>
+                {inputErr.imageErr? <Form.Text className="text-danger">{inputErr.imageErr}</Form.Text>:""}
+              </Form.Group>
+
+              {/* Submit Button */}
+              <div className="text-center">
+                <Button type="submit" className="btn border-0 submit-btn p-2 w-50">
+                  Add Product
+                </Button>
+              </div>
+            </Form>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Crud;
--- React Js/react-boostrap/src/App.jsx
+++ React Js/react-boostrap/src/App.jsx
@@ -1,32 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Col, Form, Row ,Accordion ,Container} from 'react-bootstrap'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="">
-        <button className='btn btn-primary' onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Container>
+      <Form>
+       <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+        <Form.Label column sm="1" >
+          Email:
+        </Form.Label>
+        <Col sm="4" md="6" lg="8" xl="12">
+          <Form.Control type='text'  placeholder="email@example.com" />
+        </Col>
+      </Form.Group>
+      </Form>
+
+
+        <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+    </Container>
     </>
   )
 }
